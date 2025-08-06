2a0edf68897c71b3ce37a587c5dd2b2b010fd4fd
Gabriel Isper <2411600431@fema.edu.br>
2025-08-06 16:21:38 -0300

    fix

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
+declare module '*.svg' {
+    const value: React.FC<React.SVGProps<SVGSVGElement>>;
+    export default value;
+}
